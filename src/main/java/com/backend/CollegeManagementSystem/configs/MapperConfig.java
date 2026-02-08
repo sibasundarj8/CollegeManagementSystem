3e38495acf01b0b5c062af3732f948ebb72777b9
Sibasundar Jena <sibasundarj8@gmail.com>
2026-02-08 16:46:23 +0530

    Configured Mapping strategies (Request -> Entity -> Response)

--- src/main/java/com/backend/CollegeManagementSystem/configs/MapperConfig.java
+++ src/main/java/com/backend/CollegeManagementSystem/configs/MapperConfig.java
@@ -1,5 +1,10 @@
 package com.backend.CollegeManagementSystem.configs;
 
+import com.backend.CollegeManagementSystem.dtos.*;
+import com.backend.CollegeManagementSystem.entities.AdmissionRecordEntity;
+import com.backend.CollegeManagementSystem.entities.ProfessorEntity;
+import com.backend.CollegeManagementSystem.entities.StudentEntity;
+import com.backend.CollegeManagementSystem.entities.SubjectEntity;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +14,28 @@ public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper mapper = new ModelMapper();
+
+        /*
+         * conversion path:   RequestDto --> Entity --> ResponseDto
+         */
+
+        // AdmissionRecords
+        mapper.typeMap(AdmissionRecordRequestDto.class, AdmissionRecordEntity.class);
+        mapper.typeMap(AdmissionRecordEntity.class, AdmissionRecordResponseDto.class);
+
+        // Professors
+        mapper.typeMap(ProfessorRequestDto.class, ProfessorEntity.class);
+        mapper.typeMap(ProfessorEntity.class, ProfessorResponseDto.class);
+
+        // Students
+        mapper.typeMap(StudentRequestDto.class, StudentEntity.class);
+        mapper.typeMap(StudentEntity.class, StudentResponseDto.class);
+
+        // Subjects
+        mapper.typeMap(SubjectRequestDto.class, SubjectEntity.class);
+        mapper.typeMap(SubjectEntity.class, SubjectResponseDto.class);
+
+        return mapper;
     }
 }
