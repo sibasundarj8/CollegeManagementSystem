9793fe2c8d5d50a9b9a95745aa613e1f6dec9447
Sibasundar Jena <sibasundarj8@gmail.com>
2026-02-16 19:44:32 +0530

    adding pattern matching on name in requestDTO

--- src/main/java/com/backend/CollegeManagementSystem/dtos/request/StudentRequestDto.java
+++ src/main/java/com/backend/CollegeManagementSystem/dtos/request/StudentRequestDto.java
@@ -17,7 +17,7 @@ public class StudentRequestDto {
     // use validations here not in entity...
 
     @NotBlank(message = "Title can't be blank")
-    @Pattern(regexp = "^[A-Za-z]+$", message = "Only characters allowed")
+    @Pattern(regexp = "^[A-Za-z ]+$", message = "Only characters allowed")
     @Size(min = 6, max = 30, message = "Size must be in between 6 ans 30")
     private String name;
 
